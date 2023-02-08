52d91b97c60bfaf4a6b3cb66aa961966277c75ae
chetan <ctidgam1997@gmail.com>
2023-02-08 11:17:34 +0530

    backend 1st commit

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,10 @@
-console.log('app');
+const express = require('express')
+const app = express()
+
+// app.get("/",(req,res)=>{
+//     res.send("hello express");
+// })
+
+app.listen(5000,()=>{
+    console.log('server running on port 5000');
+})
